942d5d89d71be467d9418662e4fc9931f14de2c1
Thiago Aquino <thiagoaquiino@gmail.com>
2019-08-22 12:00:07 -0300

    primeiro commit

---
+++ spring-teste/src/main/java/thiago/endpoint/StudentEndpoint.java
@@ -0,0 +1,27 @@
+package thiago.endpoint;
+
+import static java.util.Arrays.asList;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import thiago.model.Student;
+import thiago.util.DateUtil;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndpoint {
+	@Autowired
+	private DateUtil dateUtil;
+	@RequestMapping(method = RequestMethod.GET,path = "/list")
+	public List<Student> listAll(){
+		System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+		 return asList(new Student("Pedro"), new Student("Paulo"));
+	}
+	
+}
