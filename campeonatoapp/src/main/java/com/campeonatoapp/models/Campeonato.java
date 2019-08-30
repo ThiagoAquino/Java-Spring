a4a1e41297d7a9e800499a551e05e41b0cbed9c6
ThiagoAquino <thiagoaquiino@gmail.com>
2019-08-30 13:32:49 -0300

    Altarando chave primária da tabela campeonato

--- campeonatoapp/src/main/java/com/campeonatoapp/models/Campeonato.java
+++ campeonatoapp/src/main/java/com/campeonatoapp/models/Campeonato.java
@@ -18,7 +18,7 @@ public class Campeonato implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long codigo;
 	
 	private String nome;
